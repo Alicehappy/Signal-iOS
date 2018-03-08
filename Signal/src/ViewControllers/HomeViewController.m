9edd0d56d161056bddd678b1a122551778428823
Matthew Chen <charlesmchen@gmail.com>
2018-03-08 10:14:35 -0300

    Clean up cloud after successful backup export.

--- Signal/src/ViewControllers/HomeViewController.m
+++ Signal/src/ViewControllers/HomeViewController.m
@@ -421,9 +421,9 @@ typedef NS_ENUM(NSInteger, CellState) { kArchiveState, kInboxState };
 
     [self checkIfEmptyView];
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self settingsButtonPressed:nil];
-    });
+    //    dispatch_async(dispatch_get_main_queue(), ^{
+    //        [self settingsButtonPressed:nil];
+    //    });
 }
 
 - (void)viewWillDisappear:(BOOL)animated
